4449c88561b739977f95182c185738d8b78a6310
AH <AH>
2020-01-28 16:56:28 +0200

    fixed the code

--- Assets/Scripts/RedBallsCont.cs
+++ Assets/Scripts/RedBallsCont.cs
@@ -18,20 +18,12 @@ using UnityEngine;
 
      public List<GameObject> GetAllChilds()
          {
-        /*     Transform[] allChildren = GetComponentsInChildren<Transform>();
-             List<Transform> list = new List<Transform>();
-             for (int i = 0; i < GO.transform.childCount; i++)
-              {
-                  list.Add(GO.transform.GetChild(i).gameObject);
-              }
-             return list;*/
-       //     GameObject[] allChildren = GetComponentsInChildren<GameObject>();
-       //     return allChildren;
+      
              List<GameObject> list = new List<GameObject>();
              foreach (Transform child in transform)
              {
                  list.Add(child.gameObject);
-                 // do whatever you want with child transform object here
+                
              }
              return list;
     }
--- Assets/Scripts/SnookerBallController.cs
+++ Assets/Scripts/SnookerBallController.cs
@@ -24,37 +24,7 @@ public class SnookerBallController : MonoBehaviour
     void OnCollisionEnter(Collision collided)
     {
         
-      // var  CueBall = GetComponent<GameObject>();
-
-       //  var cueballPos = CueBall.transform.position;
-
-       /* if (collided.gameObject.name == "pSphere3")
-        {
-            rb.AddForce(center);
-           // Physics.gravity = new Vector3(0f, 0f, 5f);
-            //dostuff
-        }
-        if (collided.gameObject.name == "pSphere4")
-        {
-
-            rb.AddForce((center - transform.position).normalized * 10);
-          //  Physics.gravity = new Vector3(0f, 0f, -5f);
-            //dostuff
-        }
-
-        if (collided.gameObject.name == "table")
-        {
-        //    Physics.gravity = new Vector3(0f, 0f, 0f);
-      //      rb.AddForce((GameObject.Find("table").transform.position - transform.position).normalized * 10);
-       //     rb.AddForce(center * 10);
-            //dostuff
-        }
-       
-
-
-
-     //   Vector3 v = table.bounds.center;
-        int a = 0;*/
+  
     }
 
 	void FixedUpdate () {
@@ -65,10 +35,7 @@ public class SnookerBallController : MonoBehaviour
             var velocity = rigidbody.velocity;
 
             
-            /*      var pos = rigidbody.position;
-                  pos.x = redBalls.transform.position.x; //////////להמשיך מפה
-                  rigidbody.position = pos;
-                  */
+       
             velocity.y *= 1.03f;
             //  velocity.x *= 0.2f;
             rigidbody.velocity = velocity;
@@ -79,10 +46,8 @@ public class SnookerBallController : MonoBehaviour
             var velocity = rigidbody.velocity;
            
 
-            /*      var pos = rigidbody.position;
-                  pos.x = redBalls.transform.position.x; //////////להמשיך מפה
-                  rigidbody.position = pos;
-                  */
+  
+            
             velocity.x *= 1.03f;
             //  velocity.x *= 0.2f;
             rigidbody.velocity = velocity;
@@ -93,10 +58,6 @@ public class SnookerBallController : MonoBehaviour
             var velocity = rigidbody.velocity;
 
 
-            /*      var pos = rigidbody.position;
-                  pos.x = redBalls.transform.position.x; //////////להמשיך מפה
-                  rigidbody.position = pos;
-                  */
             velocity.z *= 1.03f;
             //  velocity.x *= 0.2f;
             rigidbody.velocity = velocity;
