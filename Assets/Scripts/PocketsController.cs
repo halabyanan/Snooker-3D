0b803fb101feff4e2bf3a2598660d227e8555ce4
AH <AH>
2020-02-07 18:12:38 +0200

    fixing the scorebar to appear in the sreen - checking the canvas -> rendermode->screen space - overlay

--- Assets/Scripts/PocketsController.cs
+++ Assets/Scripts/PocketsController.cs
@@ -14,14 +14,14 @@ public class PocketsController : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		foreach (var transform in redBalls.GetComponentsInChildren<Transform>()) {
 			if (transform.name == collision.gameObject.name) {
-				var objectName = collision.gameObject.name;
+				var objectName = collision.gameObject. name;
 				GameObject.Destroy(collision.gameObject);
 
 				var ballNumber = int.Parse(objectName.Replace("Ball", ""));
 				PoolGameController.GameInstance.BallPocketed(ballNumber);
 			}
 		}
-       
+        
 		if (cueBall.transform.name == collision.gameObject.name) {
 			cueBall.transform.position = originalCueBallPosition;
 		}
